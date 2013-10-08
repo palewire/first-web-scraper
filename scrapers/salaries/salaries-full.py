64390ec6f30b7a30090622e9b5837281f55b910e
Chase Davis <chase.davis@gmail.com>
2013-10-08 18:30:31 -0400

    Add some of full scraper; needs cleanup

--- scrapers/salaries/salaries-full.py
+++ scrapers/salaries/salaries-full.py
@@ -12,7 +12,6 @@ br.open(url)
 
 ########## STEP 2: Select and fill out the appropriate form ##########
 
-# Select the appropriate form, which we'll find by looking in Chrome
 br.select_form("ctl01")
 
 # Each control can be set. Dropdown lists are handled as lists, text fields take text
@@ -20,29 +19,34 @@ br.form['SearchEmployees1$CalendarYear1$ddlCalendarYear'] = ['2013']
 br.form['SearchEmployees1$ddlAgencies'] = ['931']
 br.form['SearchEmployees1$txtLastName'] = '%'
 
-# Submit the form
-br.submit()
-
 ########## STEP 3: Grab and parse the HTML ##########
 
-soup = BeautifulSoup(br.response())
-emptable = soup.find('table', id="grdEmployees")
-rows = emptable.findAll('tr')[1:]
-
-########## STEP 4: Iterate through the results and write to an output list ##########
+number_of_pages = 10
 
 output_rows = []
-for tr in rows:
+for i in range(number_of_pages + 1):
+    # Submit the form
+    br.select_form("ctl01")
+    br.form['MozillaPager1$ddlPageNumber'] = [str(i)]
+    br.submit('MozillaPager1$btnPageNumber')
 
-    output_row = []
-    for td in tr.findAll('td'):
-        output_row.append(td.text)
+    soup = BeautifulSoup(br.response())
+    emptable = soup.find('table', id="grdEmployees")
+    rows = emptable.findAll('tr')[1:]
 
-    output_rows.append(output_row)
+    for tr in rows:
+
+        output_row = []
+        for td in tr.findAll('td'):
+            output_row.append(td.text)
+
+        output_rows.append(output_row)
 
 ########## STEP 5: Write results to file ##########
 
-handle = open('out-mechanize.csv', 'a')
-outfile = csv.writer(handle)
+print output_rows
 
-outfile.writerows(output_rows)
+# handle = open('out-mechanize.csv', 'a')
+# outfile = csv.writer(handle)
+
+# outfile.writerows(output_rows)
