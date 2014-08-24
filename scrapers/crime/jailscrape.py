782138dca806122383dbba81121e07bf99afdeda
jackiekazil <jackiekazil@gmail.com>
2014-08-24 14:51:57 -0400

    Updating jailscrape with a requests version.

--- scrapers/crime/jailscrape.py
+++ scrapers/crime/jailscrape.py
@@ -8,10 +8,11 @@ url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
 html = urllib2.urlopen(url).read()
 soup = BeautifulSoup(html)
 
-# The scrape actually starts here. Let's get the table that contains the results.
+# The scrape actually starts here.
+# Let's get the table that contains the results.
 only_table = soup.find('table', attrs={'class': 'resultsTable'})
 
-output_trs = [] # The list that's going to store all of our output rows
+output_trs = []  # The list that's going to store all of our output rows
 
 # First we need to loop through all the rows in the table
 for tr in only_table.findAll('tr'):
@@ -22,7 +23,8 @@ for tr in only_table.findAll('tr'):
     # We'll store all of the values for each given row in a list
     output_tds = []
     for td in tds:
-        output_tds.append(td.text.replace('&nbsp;', '')) # Delete annoying tab character
+        # Delete annoying tab character
+        output_tds.append(td.text.replace('&nbsp;', ''))
 
     # And we'll add that list to our broader list of results
     output_trs.append(output_tds)
