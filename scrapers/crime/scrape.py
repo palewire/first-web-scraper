6163655922aeee350fe81b30048395396ffa0ce7
Ken Schwencke <schwank@gmail.com>
2023-07-18 09:30:50 -0400

    Update scrape.py with current code

--- scrapers/crime/scrape.py
+++ scrapers/crime/scrape.py
@@ -1,24 +1,23 @@
 import csv
-
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
-url = "http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp"
-response = requests.get(url)
+url = 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s?max_rows=500'
+response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = response.content
 
 soup = BeautifulSoup(html)
-table = soup.find("table", attrs={"class": "resultsTable"})
+table = soup.find('tbody', attrs={'class': 'stripe'})
 
 list_of_rows = []
-for row in table.findAll("tr")[1:]:
+for row in table.findAll('tr'):
     list_of_cells = []
-    for cell in row.findAll("td"):
-        text = cell.text.replace("&nbsp;", "")
+    for cell in row.findAll('td')[:-1]:
+        text = cell.text
         list_of_cells.append(text)
     list_of_rows.append(list_of_cells)
 
-outfile = open("./inmates.csv", "w")
+outfile = open("inmates.csv", "w")
 writer = csv.writer(outfile)
-writer.writerow(["Last", "First", "Middle", "Gender", "Race", "Age", "City", "State"])
+writer.writerow(["Last", "First", "Middle", "Suffix", "Gender", "Race", "Age", "City", "State"])
 writer.writerows(list_of_rows)
